37ce86df4f2d19f2484bb6c3c6a1594fc1301ca5
CACLR <136112842+CACLR@users.noreply.github.com>
2024-02-26 21:05:50 +0200

    whatsapp

---
+++ js/whatsapp.js
@@ -0,0 +1,3 @@
+document.getElementById('whatsapp-dealer').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/27837150580?text=Hi%20I%20am%20interested%20in%20the%202024%20Toyota%20Vitz';
+});
