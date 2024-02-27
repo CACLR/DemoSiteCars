d0617cb494eac0f2d29678e95da6388ee9f3d198
CACLR <136112842+CACLR@users.noreply.github.com>
2024-02-27 07:29:33 +0200

    Updated Value

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,3 +1,8 @@
-document.getElementById('whatsapp-dealer').addEventListener('click', function() {
-    window.location.href = 'https://wa.me/27837150580?text=Hi%20I%20am%20interested%20in%20the%202024%20Toyota%20Vitz';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var whatsappButton = document.getElementById('whatsapp-dealer');
+    whatsappButton.addEventListener('click', function() {
+      // Replace 'YOUR_URL_HERE' with the actual URL you want to navigate to
+      window.location.href = 'https://wa.me/27837150580?text=Hi%20I%20am%20interested%20in%20the%202022%20R-Line%20Tiguan';
+    });
+  });
+  
