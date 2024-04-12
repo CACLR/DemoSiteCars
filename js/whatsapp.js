398d77cc97f565d68ac642a45480665ccd726593
CACLR <136112842+CACLR@users.noreply.github.com>
2024-04-12 08:05:23 +0200

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var whatsappButton = document.getElementById('whatsapp-dealer');
     whatsappButton.addEventListener('click', function() {
       // Replace 'YOUR_URL_HERE' with the actual URL you want to navigate to
-      window.location.href = 'https://wa.me/27721735987?text=Hi%20I%20am%20interested%20in%20the%202024%20Toyota%20Vitz';
+      window.location.href = 'https://wa.me/67721735987?text=Hi%20I%20am%20interested%20in%20the%202024%20Toyota%20Vitz';
     });
   });
   
